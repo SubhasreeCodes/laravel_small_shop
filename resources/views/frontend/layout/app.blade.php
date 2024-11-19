b7484594ce4ca366bd8ed71789c8f99d07af151d
SubhasreeCodes <subhasreeps2001@gmail.com>
2024-11-19 11:28:25 +0530

    Update app.blade.php
    stylesheet, leaflet map, Favicon

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -6,15 +6,33 @@
     <title>
         @yield('title', 'Default Title')
     </title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend/imgs/theme/favicon.svg') }}" />
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{ asset('frontend/css/plugins/animate.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('frontend/css/main.css?v=5.3') }}" />
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{ asset('frontend/css/plugins/slider-range.css') }}" />
+    <link rel="stylesheet" href="{{ asset('frontend/css/main.css?v=5.3') }}" />
+
+    <!--leaflet map-->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
 </head>
 <body>
+
+    <!-- Include the Model Partial -->
+     @include('frontend.model.type1')
+
+
     <!-- Include the Header Partial -->
      @include('frontend.layout.header')
 
-    <!-- Include the Topbar Partial -->
-     @include('frontend.layout.topbar')
 
-     @yield('content')
+    @yield('content')
+
 
     <!-- Include the Footer Partial -->
      @include('frontend.layout.footer')
