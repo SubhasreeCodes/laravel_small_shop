e4190c5dd5c4d9becc0813d1d42474f9b0e2fb3a
SubhasreeCodes <subhasreeps2001@gmail.com>
2024-11-19 12:52:07 +0530

    Update AuthController.php
    Update Auth Controller with home, login, page_not_found

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,8 +6,39 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    public function home(Request $request)
+    {
+        return view('frontend/home');
+    }
     public function forget_password(Request $request)
     {
         return view('frontend/forget_password');
     }
+
+    public function login(Request $request)
+    {
+        return view('frontend/login');
+    }
+
+    public function page_not_found(Request $request)
+    {
+        return view('frontend/page_not_found/type1');
+    }
+
+    // public function login(Request $request)
+    // {
+    //     // Validation
+    //     $request->validate([
+    //         'email' => 'required|email',
+    //         'password' => 'required|string',
+    //         'security_code' => 'required|string', // Add your security code validation here
+    //     ]);
+
+    //     // Authentication logic goes here
+    //     if (auth()->attempt($request->only('email', 'password'))) {
+    //         return redirect()->route('dashboard'); // Redirect to a protected page
+    //     }
+
+    //     return back()->withErrors(['email' => 'Invalid credentials']);
+    // }
 }
